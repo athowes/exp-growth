1e6d71097163618db69fc0cddcbe628c2434bd35
athowes <adamhowes@outlook.com>
2022-06-22 15:27:40 -0400

    4PL with xBinomial processing step, all plots on flluoresence scale, automatic Stan parameter indices, 4pl and 4pl-processing into separate folders, automatic Stan plot outputs, some more background #6

--- src/fit_qPCR/analysis.R
+++ src/fit_qPCR/analysis.R
@@ -1,8 +1,16 @@
+#' Colour-blind friendly colours
 cbpalette <- c("#56B4E9", "#009E73", "#E69F00", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#999999")
 
-#' 4PL
+#' 4PL (four parameter logistic)
 
-compiled <- rstan::stan_model("4pl.stan")
+#' Based on amplification curve data I make the following judgements
+#' * The right-hand asymptote is around 8 on the "Delta Rn" scale
+#' * The number of cycles is around 40
+#' * The curves are relatively steep when they start taking off
+#' * We could fix the left-hand asymptote to zero
+
+#' Simulate data from the prior
+compiled <- rstan::stan_model("4pl/4pl.stan")
 cycles <- 20
 
 sim <- rstan::sampling(
@@ -10,90 +18,98 @@ sim <- rstan::sampling(
   data = list(T = cycles, flag_run_estimation = 0, f = rep(0, cycles))
 )
 
-sim_summary <- summary(sim)$summary %>%
+stan_par_index <- function(fit) {
+  fit@par_dims %>%
+    lapply(function(x) rep(1:ifelse(length(x) == 0, 1, x[1]))) %>%
+    unlist()
+}
+
+sim_summary <- rstan::summary(sim)$summary %>%
   as.data.frame() %>%
   tibble::rownames_to_column() %>%
-  #' I've hard coded this, better to be automatic
-  #' Somewhere in sim@par_dims
-  mutate(t = c(1, 1, 1, 1, 1, 1, 1, 1:cycles, 1:cycles, 1))
+  mutate(t = stan_par_index(sim))
 
-n_prior <- sim_summary %>%
-  filter(substr(rowname, 1, 2) == "n[")
+f_prior <- sim_summary %>%
+  filter(str_starts(rowname, "f_sim"))
 
-pdf("n-prior.pdf", h = 3, w = 6.25)
+pdf("4pl/prior.pdf", h = 3, w = 6.25)
 
-n_prior %>%
+plot_prior <- f_prior %>%
   ggplot(aes(x = t, y = mean, ymin = `2.5%`, ymax = `97.5%`)) +
-  geom_line(col = cbpalette[1], linetype = "dashed") +
+  geom_line(size = 1, col = cbpalette[1], linetype = "dashed") +
   geom_ribbon(alpha = 0.5, fill = cbpalette[1]) +
   theme_minimal() +
-  lims(y = c(0, 12)) +
-  labs(x = "t", y = "output")
+  labs(x = "t", y = "fluoresence")
+
+plot_prior
 
 dev.off()
 
-f_sim <- rstan::extract(sim, )$f_sim %>%
+pdf("4pl/prior-stan.pdf", h = 8, w = 6.25)
+
+rstan::stan_plot(sim, pars = sim@model_pars) + theme_minimal()
+rstan::stan_trace(sim, pars = sim@model_pars) + theme_minimal()
+
+dev.off()
+
+f_sim <- rstan::extract(sim)$f_sim %>%
   as.matrix()
 
-#' Example of one simulated data from the model
+#' Example of one simulated data
 f_sim[1, ]
 df_one <- data.frame(f = f_sim[1, ], t = 1:cycles)
 
-pdf("n-prior-data.pdf", h = 3, w = 6.25)
+pdf("4pl/prior-data.pdf", h = 3, w = 6.25)
 
-n_prior %>%
-  ggplot(aes(x = t, y = mean, ymin = `2.5%`, ymax = `97.5%`)) +
-  geom_line(col = cbpalette[1], linetype = "dashed") +
-  geom_ribbon(alpha = 0.5, fill = cbpalette[1]) +
-  geom_point(data = df_one, aes(x = t, y = f), inherit.aes = FALSE) +
-  theme_minimal() +
-  lims(y = c(0, 20)) +
-  labs(x = "t", y = "fluorescence")
+plot_prior_data <- plot_prior +
+  geom_point(data = df_one, aes(x = t, y = f), inherit.aes = FALSE, shape = 21)
+
+plot_prior_data
 
 dev.off()
 
-#' Let's fit the model to that data and see what it thinks the posterior would be
+#' Fit the model to that data and see what it thinks the posterior would be
 fit <- rstan::sampling(
   compiled,
   data = list(T = cycles, flag_run_estimation = 1, f = df_one$f)
 )
 
-fit_summary <- summary(fit)$summary %>%
+fit_summary <- rstan::summary(fit)$summary %>%
   as.data.frame() %>%
   tibble::rownames_to_column() %>%
-  #' I've hard coded this, better to be automatic
-  #' Somewhere in sim@par_dims
-  mutate(t = c(1, 1, 1, 1, 1, 1, 1, 1:cycles, 1:cycles, 1))
+  mutate(t = stan_par_index(fit))
 
-n_posterior <- fit_summary %>%
-  filter(substr(rowname, 1, 2) == "n[")
+f_posterior <- fit_summary %>%
+  filter(str_starts(rowname, "f_sim"))
 
-pdf("n-prior-data-posterior.pdf", h = 3, w = 6.25)
+pdf("4pl/prior-data-posterior.pdf", h = 3, w = 6.25)
 
-n_prior %>%
-  ggplot(aes(x = t, y = mean, ymin = `2.5%`, ymax = `97.5%`)) +
-  geom_line(linetype = "dashed", col = cbpalette[1], size = 1) +
-  geom_ribbon(alpha = 0.5, fill = cbpalette[1]) +
-  geom_point(
-    data = df_one, aes(x = t, y = f),
-    inherit.aes = FALSE, shape = 21
-  ) +
+plot_prior_data_posterior <- plot_prior_data +
   geom_line(
-    data = n_posterior, aes(x = t, y = mean), inherit.aes = FALSE,
+    data = f_posterior, aes(x = t, y = mean), inherit.aes = FALSE,
     linetype = "dashed", col = cbpalette[3], size = 1
   ) +
   geom_ribbon(
-    data = n_posterior, aes(x = t, ymin = `2.5%`, ymax = `97.5%`), inherit.aes = FALSE,
+    data = f_posterior, aes(x = t, ymin = `2.5%`, ymax = `97.5%`), inherit.aes = FALSE,
     fill = cbpalette[3], alpha = 0.5
-  ) +
-  theme_minimal() +
-  labs(x = "t", y = "fluorescence")
+  )
+
+plot_prior_data_posterior
 
 dev.off()
 
+pdf("4pl/posterior-stan.pdf", h = 8, w = 6.25)
+
+rstan::stan_plot(fit, pars = fit@model_pars) + theme_minimal()
+rstan::stan_trace(fit, pars = fit@model_pars) + theme_minimal()
+
+dev.off()
+
+
 #' 4PL with processing step
 
-compiled <- rstan::stan_model("4pl-processing.stan")
+#' Follow the same steps as above
+compiled <- rstan::stan_model("4pl-processing/4pl-processing.stan")
 cycles <- 20
 
 sim <- rstan::sampling(
@@ -103,11 +119,79 @@ sim <- rstan::sampling(
 
 sim_summary <- rstan::summary(sim)$summary %>%
   as.data.frame() %>%
-  tibble::rownames_to_column()
+  tibble::rownames_to_column() %>%
+  mutate(t = stan_par_index(sim))
 
-pdf("4pl-processing-stan-plots.pdf", h = 5, w = 6.25)
+f_prior <- sim_summary %>%
+  filter(str_starts(rowname, "f_sim"))
 
-rstan::stan_plot(sim)
-rstan::stan_trace(sim)
+pdf("4pl-processing/prior.pdf", h = 3, w = 6.25)
+
+plot_prior <- f_prior %>%
+  ggplot(aes(x = t, y = mean, ymin = `2.5%`, ymax = `97.5%`)) +
+  geom_line(size = 1, col = cbpalette[1], linetype = "dashed") +
+  geom_ribbon(alpha = 0.5, fill = cbpalette[1]) +
+  theme_minimal() +
+  labs(x = "t", y = "fluoresence")
+
+plot_prior
+
+dev.off()
+
+pdf("4pl-processing/prior-stan.pdf", h = 8, w = 6.25)
+
+rstan::stan_plot(sim, pars = sim@model_pars) + theme_minimal()
+rstan::stan_trace(sim, pars = sim@model_pars) + theme_minimal()
+
+dev.off()
+
+f_sim <- rstan::extract(sim)$f_sim %>%
+  as.matrix()
+
+f_sim[1, ]
+df_one <- data.frame(f = f_sim[1, ], t = 1:cycles)
+
+pdf("4pl-processing/prior-data.pdf", h = 3, w = 6.25)
+
+plot_prior_data <- plot_prior +
+  geom_point(data = df_one, aes(x = t, y = f), inherit.aes = FALSE, shape = 21)
+
+plot_prior_data
+
+dev.off()
+
+fit <- rstan::sampling(
+  compiled,
+  data = list(T = cycles, flag_run_estimation = 1, f = df_one$f)
+)
+
+fit_summary <- rstan::summary(fit)$summary %>%
+  as.data.frame() %>%
+  tibble::rownames_to_column() %>%
+  mutate(t = stan_par_index(fit))
+
+f_posterior <- fit_summary %>%
+  filter(str_starts(rowname, "f_sim"))
+
+pdf("4pl-processing/prior-data-posterior.pdf", h = 3, w = 6.25)
+
+plot_prior_data_posterior <- plot_prior_data +
+  geom_line(
+    data = f_posterior, aes(x = t, y = mean), inherit.aes = FALSE,
+    linetype = "dashed", col = cbpalette[3], size = 1
+  ) +
+  geom_ribbon(
+    data = f_posterior, aes(x = t, ymin = `2.5%`, ymax = `97.5%`), inherit.aes = FALSE,
+    fill = cbpalette[3], alpha = 0.5
+  )
+
+plot_prior_data_posterior
+
+dev.off()
+
+pdf("4pl-processing/posterior-stan.pdf", h = 8, w = 6.25)
+
+rstan::stan_plot(fit, pars = fit@model_pars) + theme_minimal()
+rstan::stan_trace(fit, pars = fit@model_pars) + theme_minimal()
 
 dev.off()
