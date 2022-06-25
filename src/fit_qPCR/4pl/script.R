c4dc31400b61810b27d94ae5f99af6def85704fe
athowes <adamhowes@outlook.com>
2022-06-24 22:36:45 -0400

    Clean up fit_qPCR by moving analysis.R into separate script.R files by folder

--- src/fit_qPCR/4pl/script.R
+++ src/fit_qPCR/4pl/script.R
@@ -32,7 +32,7 @@ sim_summary <- rstan::summary(sim)$summary %>%
   mutate(t = stan_par_index(sim))
 
 f_prior <- sim_summary %>%
-  filter(str_starts(rowname, "f_sim"))
+  filter(stringr::str_starts(rowname, "f_sim"))
 
 pdf("4pl/prior.pdf", h = 3, w = 6.25)
 
@@ -63,12 +63,12 @@ chosen_curves <- seq(from = 1, to = (100 * (n_curves - 1)) + 1, by = 100)
 f_sim_curves <- f_sim[chosen_curves, 1:40] %>%
   t() %>%
   as.data.frame() %>%
-  gather(
+  tidyr::gather(
     key = "sim",
     value = "f"
   ) %>%
   mutate(
-    sim = as.numeric(str_remove(sim, "V")),
+    sim = as.numeric(stringr::str_remove(sim, "V")),
     t = rep(1:40, length(chosen_curves)),
     observed = as.factor(c(rep(1, 40), rep(0, 40 * (n_curves - 1))))
   )
@@ -102,7 +102,7 @@ fit_summary <- rstan::summary(fit)$summary %>%
   mutate(t = stan_par_index(fit))
 
 f_posterior <- fit_summary %>%
-  filter(str_starts(rowname, "f_sim"))
+  filter(stringr::str_starts(rowname, "f_sim"))
 
 pdf("4pl/prior-data-posterior.pdf", h = 3, w = 6.25)
 
@@ -121,118 +121,6 @@ plot_prior_data_posterior
 dev.off()
 
 pdf("4pl/posterior-stan.pdf", h = 8, w = 6.25)
-
-rstan::stan_plot(fit, pars = fit@model_pars) + theme_minimal()
-rstan::stan_trace(fit, pars = fit@model_pars) + theme_minimal()
-
-dev.off()
-
-
-#' 4PL with processing step
-
-#' Follow the same steps as above
-compiled <- rstan::stan_model("4pl-processing/4pl-processing.stan")
-cycles <- 40
-
-sim <- rstan::sampling(
-  compiled,
-  data = list(T = cycles, flag_run_estimation = 0, f = rep(0, cycles))
-)
-
-sim_summary <- rstan::summary(sim)$summary %>%
-  as.data.frame() %>%
-  tibble::rownames_to_column() %>%
-  mutate(t = stan_par_index(sim))
-
-f_prior <- sim_summary %>%
-  filter(str_starts(rowname, "f_sim"))
-
-pdf("4pl-processing/prior.pdf", h = 3, w = 6.25)
-
-plot_prior <- f_prior %>%
-  ggplot(aes(x = t, y = mean, ymin = `2.5%`, ymax = `97.5%`)) +
-  geom_line(size = 1, col = cbpalette[1]) +
-  geom_ribbon(alpha = 0.2, fill = cbpalette[1]) +
-  theme_minimal() +
-  labs(x = "Cycle number", y = "Fluoresence (Delta Rn)")
-
-plot_prior
-
-dev.off()
-
-pdf("4pl-processing/prior-stan.pdf", h = 8, w = 6.25)
-
-rstan::stan_plot(sim, pars = sim@model_pars) + theme_minimal()
-rstan::stan_trace(sim, pars = sim@model_pars) + theme_minimal()
-
-dev.off()
-
-f_sim <- rstan::extract(sim)$f_sim %>%
-  as.matrix()
-
-#' Simulated curves from the prior
-n_curves <- 10
-chosen_curves <- seq(from = 1, to = (100 * (n_curves - 1)) + 1, by = 100)
-f_sim_curves <- f_sim[chosen_curves, 1:40] %>%
-  t() %>%
-  as.data.frame() %>%
-  gather(
-    key = "sim",
-    value = "f"
-  ) %>%
-  mutate(
-    sim = as.numeric(str_remove(sim, "V")),
-    t = rep(1:40, length(chosen_curves)),
-    observed = as.factor(c(rep(1, 40), rep(0, 40 * (n_curves - 1))))
-  )
-
-pdf("4pl-processing/prior-data.pdf", h = 3, w = 6.25)
-
-plot_prior_data <- plot_prior +
-  geom_line(
-    data = f_sim_curves, aes(x = t, y = f, group = sim, col = observed),
-    inherit.aes = FALSE, alpha = 0.5
-  ) +
-  scale_color_manual(values = c("grey80", "black")) +
-  labs(col = "Observed?")
-
-plot_prior_data
-
-dev.off()
-
-obs <- f_sim_curves %>%
-  filter(observed == 1)
-
-fit <- rstan::sampling(
-  compiled,
-  data = list(T = cycles, flag_run_estimation = 1, f = obs$f)
-)
-
-fit_summary <- rstan::summary(fit)$summary %>%
-  as.data.frame() %>%
-  tibble::rownames_to_column() %>%
-  mutate(t = stan_par_index(fit))
-
-f_posterior <- fit_summary %>%
-  filter(str_starts(rowname, "f_sim"))
-
-pdf("4pl-processing/prior-data-posterior.pdf", h = 3, w = 6.25)
-
-plot_prior_data_posterior <- plot_prior_data +
-  geom_line(
-    data = f_posterior, aes(x = t, y = mean), inherit.aes = FALSE,
-    col = cbpalette[3], size = 1
-  ) +
-  geom_ribbon(
-    data = f_posterior, aes(x = t, ymin = `2.5%`, ymax = `97.5%`), inherit.aes = FALSE,
-    fill = cbpalette[3], alpha = 0.5
-  )
-
-plot_prior_data_posterior
-
-dev.off()
-
-pdf("4pl-processing/posterior-stan.pdf", h = 8, w = 6.25)
 
 rstan::stan_plot(fit, pars = fit@model_pars) + theme_minimal()
 rstan::stan_trace(fit, pars = fit@model_pars) + theme_minimal()
--- src/fit_qPCR/script.R
+++ src/fit_qPCR/script.R
@@ -1,5 +1,9 @@
 # orderly::orderly_develop_start("fit_qPCR")
 # setwd("src/fit_qPCR")
 
-source("analysis.R")
+#' Run models
+source("4pl/script.R")
+source("4pl-processing/script.R")
+
+#' Generate report
 rmarkdown::render("qPCR.Rmd")
