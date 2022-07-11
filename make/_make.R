175d7c6357032fb56ba9763650b62e242b577e5b
athowes <adamhowes@outlook.com>
2022-07-11 18:25:10 -0400

    Upload CoDA notes

--- make/_make.R
+++ make/_make.R
@@ -6,3 +6,6 @@ orderly::orderly_commit(id)
 
 id <- orderly::orderly_run("explore_poisson-regression")
 orderly::orderly_commit(id)
+
+id <- orderly::orderly_run("docs_CoDA")
+orderly::orderly_commit(id)
--- utils/archive_to_docs.R
+++ utils/archive_to_docs.R
@@ -1,17 +1,20 @@
 #' Set the working directory to the project root
 setwd(rprojroot::find_rstudio_root_file())
 
-#' Name of the report to move
-report <- "explore_poisson-regression"
+archive_to_docs <- function(report) {
+  #' Artefacts to be moved
+  filenames <- yaml::read_yaml(file.path(paste0("src/", report, "/orderly.yml")))$artefacts[[1]]$data$filenames
 
-#' Artefacts to be moved
-filenames <- yaml::read_yaml(file.path(paste0("src/", report, "/orderly.yml")))$artefacts[[1]]$data$filenames
+  #' Latest version in archive
+  latest <- orderly::orderly_latest(report)
 
-#' Latest version in archive
-latest <- orderly::orderly_latest(report)
+  #' Copy files over
+  files_from <- paste0("archive/", report, "/", latest, "/", filenames)
+  files_to <- paste0("docs/", filenames)
 
-#' Copy files over
-files_from <- paste0("archive/", report, "/20220711-121610-2153998e/", filenames)
-files_to <- paste0("docs/", filenames)
+  file.copy(from = files_from, to = files_to, overwrite = TRUE)
+}
 
-file.copy(from = files_from, to = files_to, overwrite = TRUE)
+#' Names of the reports to move
+archive_to_docs("explore_poisson-regression")
+archive_to_docs("docs_CoDA")
