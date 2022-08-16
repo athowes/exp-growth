3a9a24a44ab5683a1b244702ca5c6027ebc23d03
athowes <adamhowes@outlook.com>
2022-08-16 11:50:45 -0400

    Compartmental model workshop

--- make/_make.R
+++ make/_make.R
@@ -21,3 +21,6 @@ orderly::orderly_commit(id)
 
 id <- orderly::orderly_run("fit_poisson-regression")
 orderly::orderly_commit(id)
+
+id <- orderly::orderly_run("explore_compartmental")
+orderly::orderly_commit(id)
---
+++ src/explore_compartmental/script.R
@@ -0,0 +1,4 @@
+# orderly::orderly_develop_start("explore_compartmental")
+# setwd("src/explore_compartmental/")
+
+rmarkdown::render("odin.Rmd")
